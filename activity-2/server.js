67ae64a6c52b7559a76cd664f4bb4d2250c9616e
Martin Dedja <martindedja11@gmail.com>
2024-09-08 15:30:32 -0500

    Added database connection

--- activity-2/server.js
+++ activity-2/server.js
@@ -1,11 +1,14 @@
-const express = require("express");
+import express from "express";
+import connectDB from "./config/db.js";
 
 const app = express();
 
+connectDB();
+
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+  res.send("Hello, World!");
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
