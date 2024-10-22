5ee257a3eb6c39f629ebb59c4062fbcd66427326
Martin Dedja <martindedja11@gmail.com>
2024-10-21 22:42:34 -0500

    Activity-7 implemented

--- activity-2/client/src/App.js
+++ activity-2/client/src/App.js
@@ -1,33 +1,50 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
-class App extends React.Component {
-  state = {
-    data: null,
-  };
+const App = () => {
+  const [data, setData] = useState(null);
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get("http://localhost:5000")
       .then((res) => {
-        this.setState({ data: res.data });
+        setData(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>GoodThings</h1>
-          <p>{this.state.data}</p>
-        </header>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GoodThings</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
