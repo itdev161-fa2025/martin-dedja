7da3b1e2b686d04979a8c5b0b36a10029da51019
Martin Dedja <martindedja11@gmail.com>
2024-10-21 22:12:14 -0500

    Activity - 5 implemented

--- activity-2/server.js
+++ activity-2/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import connectDB from "./config/db.js";
 import { check, validationResult } from "express-validator";
 import cors from "cors";
+import User from "./models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "config";
 
 const app = express();
 
@@ -23,12 +27,50 @@ app.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     } else {
-      return res.send("Data received");
+      const { name, email, password } = req.body;
+      try {
+        let user = await User.findOne({ email });
+        if (user) {
+          return res.status(400).json({ msg: "User already exists" });
+        }
+
+        user = new User({
+          name,
+          email,
+          password,
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+          user: {
+            id: user.id,
+          },
+        };
+
+        jwt.sign(
+          payload,
+          config.get("jwtSecret"),
+          {
+            expiresIn: "10hr",
+          },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+      }
     }
   }
 );
